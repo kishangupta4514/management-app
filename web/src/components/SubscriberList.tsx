0445a88efe3d20c6c91b12711a12b1a514b89c12
Kishan Gupta <kishangupta4514@gmail.com>
2024-11-09 18:38:16 +0530

    auth

--- web/src/components/SubscriberList.tsx
+++ web/src/components/SubscriberList.tsx
@@ -33,6 +33,7 @@ export function SubscriberList() {
     };
 
     fetchSubscriptions();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   if (error) return <p className="error">{error}</p>;
